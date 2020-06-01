c94bc1b0eab1987af5e7e859dd7007758ca4e7bf
Erich Richter <erich.richter@gmail.com>
2020-06-01 15:06:25 -0700

    create initial files > > Co-authored-by: Cody Fritz <clanalia55@gmail.com> Co-authored-by: Kyle James <kyle.james.hubbard@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+jQuery("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
+  
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+
 });
