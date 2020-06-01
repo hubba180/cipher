07c6ee2c4cddb8623c402b5c7ad8e028aaf7ef8d
Erich Richter <erich.richter@gmail.com>
2020-06-01 16:10:00 -0700

    add javascript functionality > > Co-authored-by: Cody Fritz <clanalia55@gmail.com> Co-authored-by: Kyle James <kyle.james.hubbard@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,25 @@ jQuery("h1").click(function() {
   $("img").click(function() {
     alert("This is an image.");
   });
-
+  
 });
+
+
+let userInput = prompt("enter a sentence");
+
+function reversed(){
+    
+   //let splitString = userInput.split("");
+    
+   let firstChar = userInput.charAt(0);
+  let stringLength = userInput.length;
+  let lastChar = userInput.charAt(stringLength-1);
+    //let reverseString = splitString.reverse();
+   let firstLast = firstChar + lastChar;
+    return userInput + firstLast;
+}
+
+console.log(reversed());
+
+  
+
