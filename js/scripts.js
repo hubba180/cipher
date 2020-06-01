f67ff0d73593869ebd3097d6c40cbc59dd126969
Erich Richter <erich.richter@gmail.com>
2020-06-01 16:32:50 -0700

    add javascript functionality > > Co-authored-by: Cody Fritz <clanalia55@gmail.com> Co-authored-by: Kyle James <kyle.james.hubbard@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,23 @@ let userInput = prompt("enter a sentence");
 
 function reversed(){
     
-   //let splitString = userInput.split("");
-    
-   let firstChar = userInput.charAt(0);
-  let stringLength = userInput.length;
-  let lastChar = userInput.charAt(stringLength-1);
-    //let reverseString = splitString.reverse();
-   let firstLast = firstChar + lastChar;
-    return userInput + firstLast;
+    let firstChar = userInput.charAt(0);
+    let stringLength = userInput.length;
+    let lastChar = userInput.charAt(stringLength-1);
+    let firstLast = firstChar + lastChar;
+    let splitStr = firstLast.split("")
+    let  firstLastReversed = splitStr.reverse();
+    let joinedStr = firstLastReversed.join("");
+    let caps = joinedStr.toUpperCase();
+    let charCount = stringLength
+    let charDivide = Math.round(charCount/2);
+
+    return charDivide + userInput + caps;
 }
+   
+    
+
+
 
 console.log(reversed());
 
