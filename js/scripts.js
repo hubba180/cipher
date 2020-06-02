b1402ccb856d40191e40330c827ef0607d3aaee2
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-02 11:05:29 -0700

    add jQuery functions to toggle walrus picture
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,7 @@
 $(document).ready(function(){
-
-jQuery("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
-  
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-  
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-  
 });
-
-
-let userInput = prompt("enter a sentence");
-
-function reversed(){
-    
-    let firstChar = userInput.charAt(0);
-    let stringLength = userInput.length;
-    let lastChar = userInput.charAt(stringLength-1);
-    let firstLast = firstChar + lastChar;
-    let splitStr = firstLast.split("")
-    let  firstLastReversed = splitStr.reverse();
-    let joinedStr = firstLastReversed.join("");
-    let caps = joinedStr.toUpperCase();
-    let charCount = stringLength
-    let charDivide = Math.round(charCount/2);
-
-    return charDivide + userInput + caps;
-
-
-}
-   
-    
-
-
-console.log(reversed());
-
-  
 
